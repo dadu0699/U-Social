c9f9b8adf0071592ea70c3a809eb83d65d157708
dadu0699 <dadu0699@hotmail.com>
2021-10-16 19:06:39 -0600

    [ADD] Settings for Rekognition

---
+++ Backend/config/rekognition.js
@@ -0,0 +1,34 @@
+const Rekognition = require('aws-sdk/clients/rekognition');
+
+require('dotenv').config();
+const {
+  S3_BUCKET,
+  REKOGNITION_REGION,
+  REKOGNITION_ACCESS_KEY_ID,
+  REKOGNITION_SECRET_ACCESS_KEY,
+} = process.env;
+
+const client = new Rekognition({
+  region: REKOGNITION_REGION,
+  accessKeyId: REKOGNITION_ACCESS_KEY_ID,
+  secretAccessKey: REKOGNITION_SECRET_ACCESS_KEY,
+});
+
+const compareFaces = async (base64Image, imageS3) => {
+  const params = {
+    SourceImage: {
+      Bytes: Buffer.from(base64Image, 'base64'),
+    },
+    TargetImage: {
+      S3Object: {
+        Bucket: S3_BUCKET,
+        Name: imageS3,
+      },
+    },
+    SimilarityThreshold: '80',
+  };
+
+  return await client.compareFaces(params).promise();
+};
+
+module.exports = { compareFaces };
