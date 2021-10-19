4a9a68d45eb3b4b096f24a0eac7063ca0d9fc8bc
dadu0699 <dadu0699@hotmail.com>
2021-10-18 20:54:32 -0600

    [ADD] Chat page functionality

--- Frontend/src/app/pages/chat/chat.component.ts
+++ Frontend/src/app/pages/chat/chat.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+import { User } from 'src/app/models/user.model';
+import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
+  public user!: User;
+  public chats: any[];
+  public chat!: any;
+  public content!: string | undefined;
 
-  constructor() { }
+  private subscription!: Subscription;
 
-  ngOnInit(): void {
+  constructor(private _chatService: ChatService) {
+    this.chats = [];
   }
 
+  async ngOnInit(): Promise<void> {
+    await this.getChats();
+
+    const source = interval(2500);
+    this.subscription = source.subscribe(() => this.dataChat());
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
+  private async getChats() {
+    try {
+      const response = await this._chatService.getChats();
+      if (response['code'] === 200) {
+        this.chats = response['data'];
+      }
+    } catch (err) {
+      console.log(<any>err);
+    }
+  }
+
+  public async getChat(chat: any) {
+    this.chat = chat;
+    await this.dataChat();
+  }
+
+  public async dataChat() {
+    await this.getChats();
+
+    if (this.chat) {
+      try {
+        const response = await this._chatService.getMessages(this.chat.userID);
+        if (response['code'] === 200) {
+          this.chat.messages = response['data'];
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  public async sendMessage() {
+    if (this.content) {
+      try {
+        await this._chatService.sendMessage({
+          content: this.content,
+          friendID: this.chat.userID,
+        });
+        this.content = undefined;
+
+        await this.dataChat();
+      } catch (err) {
+        console.log(<any>err);
+      }
+    }
+  }
 }
--- Frontend/src/app/services/chat.service.ts
+++ Frontend/src/app/services/chat.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
+  public url: string;
+  private user!: User;
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) {
+    this.url = `${environment.url}/chat`;
+  }
+
+  public getUser(): User | undefined {
+    const user: string | null = localStorage.getItem('user');
+    if (user !== null) return <User>JSON.parse(user);
+    return undefined;
+  }
+
+  private currentUser(): void {
+    const user: User | undefined = this.getUser();
+    if (user !== undefined) {
+      this.user = user;
+    }
+  }
+
+  public async getChats(): Promise<any> {
+    this.currentUser();
+    const params = new HttpParams().set('userID', this.user.userID);
+
+    return await this._httpClient
+      .get(`${this.url}/all`, { params })
+      .toPromise();
+  }
+
+  public async sendMessage(message: any): Promise<any> {
+    this.currentUser();
+    message.userID = this.user.userID;
+    const json = JSON.stringify(message);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return await this._httpClient.post(this.url, json, { headers }).toPromise();
+  }
+
+  public async getMessages(friendID: number): Promise<any> {
+    this.currentUser();
+    const params = new HttpParams()
+      .set('userID', this.user.userID)
+      .set('friendID', friendID);
+
+    return await this._httpClient.get(this.url, { params }).toPromise();
+  }
 }
